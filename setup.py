acea9ec313a7349ac354a738c7526be46696cfe8
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-04-13 12:57:52 -0300

    Updated versions

--- setup.py
+++ setup.py
@@ -18,7 +18,8 @@ install_requires = \
  'git+https://github.com/exasol/error-reporting-python.git@main',
  'exasol_script_languages_container_tool @ '
  'https://github.com/exasol/script-languages-container-tool/releases/download/0.10.0/exasol_script_languages_container_tool-0.10.0-py3-none-any.whl',
- 'jinja2>=3.0.0']
+ 'jinja2>=3.0.0',
+ 'jsonschema>=3.2.0,<4.0.0']
 
 setup_kwargs = {
     'name': 'exasol-script-languages-container-ci-setup',
