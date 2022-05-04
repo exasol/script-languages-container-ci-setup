7d474793e55ff23f2ad3c1fee8d99ab4612022a6
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-05-04 15:27:27 -0300

    Fixed import of new commands

--- exasol_script_languages_container_ci_setup/__init__.py
+++ exasol_script_languages_container_ci_setup/__init__.py
@@ -3,6 +3,8 @@ from exasol_script_languages_container_ci_setup.cli.commands import (
     generate_buildspec,
     deploy_source_credentials,
     deploy_ci_build,
+    deploy_release_build,
     validate_ci_build,
+    validate_release_build,
     validate_source_credentials
 )
