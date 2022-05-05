cf53a0f4ab2550c0a3cc312ade01b943eec056bd
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-05-05 06:44:59 -0300

    Added generate-release-buildspecs to import list

--- exasol_script_languages_container_ci_setup/__init__.py
+++ exasol_script_languages_container_ci_setup/__init__.py
@@ -1,6 +1,7 @@
 from exasol_script_languages_container_ci_setup.cli.commands import (
     health,
     generate_buildspec,
+    generate_release_buildspec,
     deploy_source_credentials,
     deploy_ci_build,
     deploy_release_build,
