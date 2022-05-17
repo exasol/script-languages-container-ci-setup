f4979db99133ce72f194ea7814b7a1d6e77751df
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-05-17 12:30:41 -0300

    Added documentation

--- exasol_script_languages_container_ci_setup/__init__.py
+++ exasol_script_languages_container_ci_setup/__init__.py
@@ -9,5 +9,6 @@ from exasol_script_languages_container_ci_setup.cli.commands import (
     validate_release_build,
     validate_source_credentials,
     start_release_build,
-    start_test_release_build
+    start_test_release_build,
+    start_ci_build
 )
