0455f193c7b8870a6ca3b5a7db971fcaa000d926
Torsten Kilias <tkilias@users.noreply.github.com>
2023-03-22 08:16:21 +0100

    #39: Update slc-ci to 0.6.0 and remove dependency to exaslct (#40)
    * Update slc-ci to 0.6.0 and remove dependency to exaslct #38 
    * Increase version number
    
    Co-authored-by: Christoph Pirkl <christoph.pirkl@exasol.com>

--- setup.py
+++ setup.py
@@ -16,17 +16,15 @@ install_requires = \
  'boto3>=1.22.0,<2.0.0',
  'click>=8.1.3,<9.0.0',
  'exasol-script-languages-container-ci @ '
- 'https://github.com/exasol/script-languages-container-ci/releases/download/0.5.0/exasol_script_languages_container_ci-0.5.0-py3-none-any.whl',
+ 'https://github.com/exasol/script-languages-container-ci/releases/download/0.6.0/exasol_script_languages_container_ci-0.6.0-py3-none-any.whl',
  'exasol_error_reporting_python @ '
  'git+https://github.com/exasol/error-reporting-python.git@0.2.0',
- 'exasol_script_languages_container_tool @ '
- 'https://github.com/exasol/script-languages-container-tool/releases/download/0.15.0/exasol_script_languages_container_tool-0.15.0-py3-none-any.whl',
  'jinja2>=3.1.0',
- 'jsonschema>=3.2.0,<4.0.0']
+ 'jsonschema>=4.17.3,<5.0.0']
 
 setup_kwargs = {
     'name': 'exasol-script-languages-container-ci-setup',
-    'version': '0.7.0',
+    'version': '0.8.0',
     'description': 'Manages AWS cloud CI build infrastructure.',
     'long_description': 'None',
     'author': 'Thomas Uebensee',
