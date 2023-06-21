d239f185e3c0ee03e138af6a67c03ffb9d1aca65
Torsten Kilias <torsten.kilias@exasol.com>
2023-06-21 16:53:25 +0200

    Apply review findings

--- test/unit_tests/test_config_data_model_generator.py
+++ test/unit_tests/test_config_data_model_generator.py
@@ -3,8 +3,8 @@ from pathlib import Path
 
 import pytest
 
-from exasol_script_languages_container_ci_setup.lib.config.pydantic_model_generator import generate_config_data_model, \
-    CONFIG_DATA_MODEL_FILE_NAME
+from exasol_script_languages_container_ci_setup.lib.config.data_model_generator import CONFIG_DATA_MODEL_FILE_NAME, \
+    generate_config_data_model
 
 
 @pytest.fixture
@@ -14,7 +14,7 @@ def config_data_model_file(tmp_path) -> Path:
     yield output_file
 
 
-def test(config_data_model_file):
+def test_config_data_model_generation(config_data_model_file):
     import importlib.util
     module_name = "test_create_model_can_be_imported"
     spec = importlib.util.spec_from_file_location(module_name, config_data_model_file)
@@ -35,3 +35,4 @@ def test(config_data_model_file):
             timeout_in_minutes=1
         )
     )
+    assert True
