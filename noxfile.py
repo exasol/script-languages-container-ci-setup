27d19cb7af9092e59ee898819548566ea27ca48b
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-05-16 08:51:13 +0200

    Added task `update-integration-test-github-workflows` to noxfile.pye

--- noxfile.py
+++ noxfile.py
@@ -5,3 +5,12 @@ from exasol.toolbox.nox.tasks import *
 
 # default actions to be run if nothing is explicitly specified with the -s option
 nox.options.sessions = ["project:fix"]
+
+
+@nox.session(name="update-integration-test-github-workflows", python=False)
+def update_integration_test_github_workflows(session: nox.Session):
+    """
+    Updates the Github workflows under ".github/workflows" for the dummy SLC in this repository.
+    """
+    session.run("exaslc-ci-setup", "deploy-ci-build")
+    session.run("git", "add", ".github/workflows/*.yml")
