e803b48e7a5d8c2725ac2e52b94aa1ee5fe727d6
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-05-20 13:32:02 +0200

    #91: Created new templates for Github Workflows

--- exasol/slc_ci_setup/lib/deploy_ci_build.py
+++ exasol/slc_ci_setup/lib/deploy_ci_build.py
@@ -5,8 +5,18 @@ from exasol.slc_ci_setup.lib.render_template import render_template
 
 def deploy_ci_build():
     target_path = Path() / ".github" / "workflows"
-    templates = ["slc_ci.yml", "slc_ci_self_check.yml"]
+    templates = [
+        "slc_ci.yml.tmpl",
+        "slc_ci_self_check.yml.tmpl",
+        "slc_ci_build_slc.yml.tmpl",
+        "slc_ci_check_for_build.yml.tmpl",
+        "slc_ci_flavor.yml.tmpl",
+        "slc_ci_prepare_test_container.yml.tmpl",
+        "slc_ci_test_slc.yml.tmpl",
+    ]
     for template in templates:
         github_workflow = render_template(template)
-        with open(target_path / template, "w", encoding="utf-8") as f:
+        with open(
+            target_path / template.replace(".yml.tmpl", ".yml"), "w", encoding="utf-8"
+        ) as f:
             f.write(github_workflow)
