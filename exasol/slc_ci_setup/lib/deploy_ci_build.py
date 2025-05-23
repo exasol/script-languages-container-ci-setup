082e71ed1ba2e12d196b191f48d0574ff55d1025
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-05-23 08:43:06 +0200

    uses jinja2 include directive

--- exasol/slc_ci_setup/lib/deploy_ci_build.py
+++ exasol/slc_ci_setup/lib/deploy_ci_build.py
@@ -1,22 +1,18 @@
 from pathlib import Path
 
-from exasol.slc_ci_setup.lib.render_template import render_template
+from exasol.slc_ci_setup.lib.template_access import (
+    list_templates,
+    render_template,
+)
 
 
 def deploy_ci_build():
     target_path = Path() / ".github" / "workflows"
-    templates = [
-        "slc_ci.yml.tmpl",
-        "slc_ci_self_check.yml.tmpl",
-        "slc_ci_build_slc.yml.tmpl",
-        "slc_ci_check_for_build.yml.tmpl",
-        "slc_ci_flavor.yml.tmpl",
-        "slc_ci_prepare_test_container.yml.tmpl",
-        "slc_ci_test_slc.yml.tmpl",
-    ]
-    heading = render_template("heading.tmpl")
+    templates = list_templates("slc_ci")
+
     for template in templates:
-        github_workflow = render_template(template, heading=heading)
+        print(template)
+        github_workflow = render_template(template)
         with open(
             target_path / template.replace(".yml.tmpl", ".yml"), "w", encoding="utf-8"
         ) as f:
