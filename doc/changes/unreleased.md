481554535a5b8df8f9865446bcf09e821d29e36c
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-06-06 09:17:31 +0200

    Prepare release 2.0.3

--- doc/changes/unreleased.md
+++ doc/changes/unreleased.md
@@ -1,5 +1,1 @@
 # Unreleased
-
-## Bugs
-
- - #133: Fix slc_ci_prepare_test_container github workflow
--- exasol/slc_ci_setup/version.py
+++ exasol/slc_ci_setup/version.py
@@ -6,6 +6,6 @@
 # If you need to change the version, do so in the pyproject.toml, e.g. by using `poetry version X.Y.Z`.
 MAJOR = 2
 MINOR = 0
-PATCH = 2
+PATCH = 3
 VERSION = f"{MAJOR}.{MINOR}.{PATCH}"
 __version__ = VERSION
