3169ce56e5a4768e4613be391c304e09518abdc3
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-06-10 08:55:43 +0200

    #138: Remove legacy AWS CI-CD (#139)
    fixes #138

--- exasol_script_languages_container_ci_setup/cli/options/aws_options.py
+++
@@ -1,7 +0,0 @@
-import click
-
-aws_options = [
-    click.option(
-        "--aws-profile", required=False, type=str, help="Id of the AWS profile to use."
-    ),
-]
---
+++ test/integration/test_integration.py
@@ -0,0 +1,8 @@
+"""
+Integration tests in this repository work by having a test-flavor under the "./flavors" directory,
+the "build_config.json" and the Github workflows (which are the output of exaslc-ci-setup).
+"""
+
+
+def test_dummy():
+    pass
